808e7aacf6c7d2753d368a0ecdc645222bcb7f15
LucasVon0645 <lucas.vongarcia@gmail.com>
2021-03-02 08:32:42 -0300

    Add: Landing Page, HeaderComponent

---
+++ src/pages/TeacherList/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './styles.css'
+import PageHeader from '../../components/pageHeader';
+
+export default function TeacherList() {
+    return (
+        <div id="page-teacher-list" className="container" >
+                <PageHeader/>
+        </div>
+    )
+}
