33dc409c71cd8dd75e959f3ea1bca12f78b14b96
LucasVon0645 <lucas.vongarcia@gmail.com>
2021-03-02 09:42:25 -0300

    Update: Header responsivo na TeacherList

--- src/components/pageHeader/index.tsx
+++ src/components/pageHeader/index.tsx
@@ -5,11 +5,13 @@ import { Link } from 'react-router-dom';
 import backIcon from '../../assets/images/icons/back.svg';
 import logoImg from '../../assets/images/logo.svg';
 
+import "./styles.css"
+
 interface PageHeaderProps {
     title: string;
 }
 
-const PageHeader: React.FC<PageHeaderProps> = ({title}) => {
+const PageHeader: React.FC<PageHeaderProps> = (props) => {
     return (
         <header className="page-header">
                 <div className="top-bar-container">
@@ -23,7 +25,8 @@ const PageHeader: React.FC<PageHeaderProps> = ({title}) => {
                 </div>
                 
                 <div className="header-content">
-                    <strong>{title}</strong>
+                    <strong>{props.title}</strong>
+                    {props.children}
                 </div>
 
             </header>
--- src/pages/TeacherList/index.jsx
+++ src/pages/TeacherList/index.jsx
@@ -5,7 +5,24 @@ import PageHeader from '../../components/pageHeader';
 export default function TeacherList() {
     return (
         <div id="page-teacher-list" className="container" >
-                <PageHeader/>
+                <PageHeader title="Esses são os proffys disponíveis">
+                    <form id="search-teachers">
+                        <div className="input-block">
+                            <label htmlFor="subject">Matéria</label>
+                            <input type="text" id="subject"></input>
+                        </div>
+
+                        <div className="input-block">
+                            <label htmlFor="week_day">Dia da semana</label>
+                            <input type="text" id="week_day"></input>
+                        </div>
+
+                        <div className="input-block">
+                            <label htmlFor="time">Matéria</label>
+                            <input type="text" id="time"></input>
+                        </div>
+                    </form>
+                </PageHeader>
         </div>
     )
 }
